08c7566e39df0e4c90b6d77c44b735ed9297244a
WorkBuddy <workbuddy@local>
2026-08-07 18:39:50 +0800

    Revert "perf(img): 文章图片与卡片预览图改用原生懒加载"
    This reverts commit d47423d99691a1cc624e6d550125a147cf95f6da.

--- client/src/components/markdown.tsx
+++ client/src/components/markdown.tsx
@@ -101,8 +101,6 @@ function MarkdownImage({
         alt={alt}
         width={width}
         height={height}
-        loading="lazy"
-        decoding="async"
         onClick={() => {
           show(cleanSrc);
         }}
